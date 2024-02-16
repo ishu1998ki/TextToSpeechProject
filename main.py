c4a6e9dd7b6d5d24b0fe1acd20b6e6bd11acd0c4
ishu1998ki <kishari1998@gmail.com>
2024-02-16 11:18:36 +0530

    edited code for read pages one by one and create separate .mp3 file for each.

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ text_speech = pyttsx3.init()
 # rate = text_speech.getProperty('rate')
 
 # to set a rate property
-text_speech.setProperty('rate',125)
+text_speech.setProperty('rate', 125)
 
 # to change voice, male for index [0], female for index [1]
 voices = text_speech.getProperty('voices')
-text_speech.setProperty('voice',voices[1].id)
+text_speech.setProperty('voice', voices[1].id)
 
 # creating a pdf file object
 pdfFileObj = open('ass.pdf', 'rb')
@@ -19,28 +19,24 @@ pdfFileObj = open('ass.pdf', 'rb')
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfReader(pdfFileObj)
 
+lengthOfPDF = len(pdfReader.pages)
 # printing number of pages in pdf file
-# print("Number of pages in this pdf = " +len(pdfReader.pages))
+print("Number of pages in this pdf = " + str(lengthOfPDF))
 
-# creating a page object
-pageObj = pdfReader.pages[1]
+# creating a loop for read pages one by one, and create a separate
+# audio files for each page.
 
-# to extract content in the page
-extractedText = pageObj.extract_text()
+for i in range(lengthOfPDF):
+    pageObj = pdfReader.pages[i]
+    extractedText = pageObj.extract_text()
+    # remove newlines
+    cleaned_text = list(map(lambda x: x.strip('\n'), extractedText))
+    result_string = ''.join(cleaned_text)
+    # saving in separate audio file
+    text_speech.save_to_file(result_string, "page_"+str(i)+'.mp3')
 
-# to remove line breaks in the text
-cleaned_text = list(map(lambda x: x.strip('\n'), extractedText))
-
-# Join the characters to form a single string
-result_string = ''.join(cleaned_text)
-print(result_string)
-# print(cleaned_text)
-text_speech.say(result_string)
-
-text_speech.save_to_file(result_string,'output.mp3')
 text_speech.runAndWait()
 
 # closing the pdf file object
 pdfFileObj.close()
-
-
+print("Successfull!")
